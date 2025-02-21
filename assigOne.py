cd66f1e699e0e7291d2449ad08619da4be05815c
Mohammad Assidi <mhmdassidi2002@egmail.com>
2025-02-21 18:49:15 +0200

    calculating and displaying amount allocated to savings, rent, and electricity.

--- assigOne.py
+++ assigOne.py
@@ -1,8 +1,18 @@
 salary=int(input("input your salary for the month: "))
 month=input(" input the name of the month you are storing the salary for: ")
-savings=int(input("input the percentage for savings:"))
-savings=int(input("input the percentage for rent: "))
-savings=int(input("input the percentage for electricity: "))
+savingsPercent=int(input("input the percentage for savings:"))
+rentPercent=int(input("input the percentage for rent: "))
+electPercent=int(input("input the percentage for electricity: "))
+
+savingsAmount=(savingsPercent/100)*salary
+rentAmount=(rentPercent/100)*salary
+electAmount=(electPercent/100)*salary
+
+print("The amount allocated to savings is: "+str(savingsAmount))
+print("The amount allocated to rent is: "+str(rentAmount))
+print("The amount allocated to electricity is: "+str(electAmount))
+
+
 
 
 
