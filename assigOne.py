702bc8eacdcea06c9915f92a4d00f95e8e58b362
Mohammad Assidi <mhmdassidi2002@egmail.com>
2025-02-21 18:29:54 +0200

    Aking the user to input the salar, month storing salary for and percentages for savings,rent and electricity

---
+++ assigOne.py
@@ -0,0 +1,8 @@
+salary=int(input("input your salary for the month: "))
+month=input(" input the name of the month you are storing the salary for: ")
+savings=int(input("input the percentage for savings:"))
+savings=int(input("input the percentage for rent: "))
+savings=int(input("input the percentage for electricity: "))
+
+
+
